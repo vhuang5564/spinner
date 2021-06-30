8e1ceba338cef0988a4cd62c4d08ce90e6ddb0be
vhuang5564 <vhuang5564@gmail.com>
2021-06-30 06:11:17 +0000

    added comments

--- spinner2.js
+++ spinner2.js
@@ -1,11 +1,11 @@
 const spinny = () => {
 
   let revs = 0;
-  const spinnyThing = ['|', '/', '-', '\\','|', '/', '-', '\\', '|', '/', '-', '\\' ];
+  const spinnyThing = ['|', '/', '-', '\\','|', '/', '-', '\\', '|', '/', '-', '\\' ]; // goes through each array. added backslash to backslash syntax.
   spinnyThing.forEach(spins => {
     setTimeout(() => {
-      process.stdout.write('\r' + spins);
-    }, revs = revs + 200);
+      process.stdout.write('\r' + spins); // \r will delete spins
+    }, revs = revs + 200); // duration of spins
   });
 };
 
